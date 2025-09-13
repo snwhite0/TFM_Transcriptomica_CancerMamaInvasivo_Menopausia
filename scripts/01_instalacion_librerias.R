f2683dcf625619822ed4a65d1d7f5af31bacd0e7
snwhite0 <nsaras6@outlook.es>
2025-09-13 14:15:23 +0200

    01_instalacion_librerias.R

---
+++ scripts/01_instalacion_librerias.R
@@ -0,0 +1,56 @@
+# Paquetes CRAN
+paquetes_cran = c("data.table", "tidyverse", "viridis", "ggrepel","UpSetR", "pheatmap", "RColorBrewer", "enrichR", "survival")
+
+# Paquetes Bioconductor
+paquetes_bioc = c("edgeR", "limma", "topGO", "AnnotationDbi", "org.Hs.eg.db", "GO.db", "Rgraphviz", "dorothea", "viper")
+
+# Función para instalar paquetes CRAN si faltan
+instalar_si_falta = function(libreria) {
+  if (!requireNamespace(libreria, quietly = TRUE)) {
+    install.packages(libreria)
+  }
+}
+
+# Función para instalar paquetes Bioconductor si faltan
+instalar_bioconductor_si_falta = function(libreria) {
+  if (!requireNamespace("BiocManager", quietly = TRUE)) {
+    install.packages("BiocManager")
+  }
+  if (!requireNamespace(libreria, quietly = TRUE)) {
+    BiocManager::install(libreria, update = FALSE, ask = FALSE)
+  }
+}
+
+# Instalar paquetes si faltan
+invisible(sapply(paquetes_cran, instalar_si_falta))
+invisible(sapply(paquetes_bioc, instalar_bioconductor_si_falta))
+cat("¡Listo! Las librerías necesarias han sido instaladas y cargadas correctamente.\n")
+
+# Cargar paquetes silenciosamente
+suppressPackageStartupMessages({
+  suppressWarnings({
+    sapply(c(paquetes_cran, paquetes_bioc), require, character.only = TRUE)
+    library(grid)
+    library(gridExtra)
+  })
+})
+
+# Función para obtener versiones y origen
+info_paquetes = function(lista, origen) {
+  data.frame(
+    Paquete = lista,
+    Version = sapply(lista, function(p) as.character(packageVersion(p))),
+    Origen = origen,
+    stringsAsFactors = FALSE
+  )
+}
+
+# Generar tabla con versiones y guardar
+df_cran = info_paquetes(paquetes_cran, "CRAN")
+df_bioc = info_paquetes(paquetes_bioc, "Bioconductor")
+df_otros = info_paquetes(c("grid", "gridExtra"), "Base/Extra")
+df_total = rbind(df_cran, df_bioc, df_otros) # Combinar todo
+
+write.csv(df_total, file = "versiones_paquetes_cargados.csv", row.names = FALSE)
+cat("Se ha generado el archivo 'versiones_paquetes_cargados.csv'.\n")
+    
