73bd814593ffdf9cf3bc8e27ec11cde65d08b645
snwhite0 <nsaras6@outlook.es>
2025-09-17 10:33:04 +0200

    Update 05_upset_plots.R

--- scripts/05_upset_plots.R
+++ scripts/05_upset_plots.R
@@ -1,4 +1,4 @@
-# 6.1. Definir directorios
+### Upset Plots
 resultados_dir = "../resultados/6.Superposicion_DEGs"
 upset_plots_dir = file.path(resultados_dir, "upset_plots")
 
@@ -6,6 +6,8 @@ if (!dir.exists(upset_plots_dir)) {
   dir.create(upset_plots_dir, recursive = TRUE)
 }
 
+## Upset Plots para los resultados del DEA de cada objetivo (total = 2)
+# Crear función para generar y guardar los plots
 crear_y_guardar_upset_plot = function(lista_genes_significativos, nombre_objetivo) { 
   if (length(lista_genes_significativos) > 1 && sum(sapply(lista_genes_significativos, length) > 0) >= 2) {
     genes_for_upset = lista_genes_significativos[sapply(lista_genes_significativos, length) > 0]
@@ -33,7 +35,6 @@ crear_y_guardar_upset_plot = function(lista_genes_significativos, nombre_objetiv
     
     print(upset_plot)
 
-    # Guardar PDF
     archivo_pdf_upset = file.path(upset_plots_dir, paste0("upset_plot_", nombre_objetivo, ".pdf"))
     pdf(file = archivo_pdf_upset, width = 12, height = 10)
     print(upset_plot)
@@ -49,21 +50,18 @@ crear_y_guardar_upset_plot = function(lista_genes_significativos, nombre_objetiv
   }
 }
 
-# 6.3. Ejecutar la función para cada objetivo
+# Ejecutar función para cada objetivo
 cat("Upset Plot: DEGs Compartidos y Específicos en Obj1"); crear_y_guardar_upset_plot(ids_genes_significativos_obj1, "obj1")
 cat("Upset Plot: DEGs Compartidos y Específicos en Obj2"); crear_y_guardar_upset_plot(ids_genes_significativos_obj2, "obj2")
 
-# 6.4. Generar Upset Plots individuales para cada subtipo (objetivo 2)
+## Upset Plots para los resultados del DEA por subtipo molecular del objetivo 2 (total = 4)
 target_subtypes = c("Basal", "Her2","LumA", "LumB")
 
 for (subtipo in target_subtypes) {
-  cat("Upset Plot: DEGs Compartidos y Específicos en el subtipo", subtipo)
-  
-  # Filtrar listas por subtipo
-  listas_subtipo = ids_genes_significativos_obj2[grep(paste0("^", subtipo, "_"), names(ids_genes_significativos_obj2))]
-  
-  # Eliminar listas vacías
-  listas_subtipo = listas_subtipo[sapply(listas_subtipo, length) > 0]
+  cat("Upset Plot: DEGs compartidos en el subtipo", subtipo)
+
+  listas_subtipo = ids_genes_significativos_obj2[grep(paste0("^", subtipo, "_"), names(ids_genes_significativos_obj2))] # Filtrar resultados por subtipo
+  listas_subtipo = listas_subtipo[sapply(listas_subtipo, length) > 0]   # Eliminar listas vacías
   
   if (length(listas_subtipo) >= 2) {
     upset_obj2 = upset(fromList(listas_subtipo),
@@ -88,8 +86,7 @@ for (subtipo in target_subtypes) {
     )
     
     print(upset_obj2)
-    
-    # Guardar el plot
+
     pdf(file = file.path(upset_plots_dir, paste0("upset_plot_obj2_", subtipo, ".pdf")),
     width = 12, height = 9)
     
@@ -97,5 +94,3 @@ for (subtipo in target_subtypes) {
     dev.off()
   }
 }
-
-cat("¡Listo! Todos los UpSet Plots han sido generados y guardados en:\n", upset_plots_dir, "\n")
